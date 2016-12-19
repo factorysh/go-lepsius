117e607931b4b61df72a8962256aa6af3ce25a94
Mathieu Lecarme <mlecarme@bearstech.com>
2016-12-19 11:59:14 +0100

    Fix

--- cli/lepsius/main.go
+++ cli/lepsius/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/bearstech/go-lepsius"
 	"gopkg.in/mcuadros/go-syslog.v2"
 )
 
 func main() {
-
-	handler := lepsius.NewHandler("%{HAPROXYHTTP}")
+	handler, err := lepsius.NewHandler("%{HAPROXYHTTPDIRECT}")
+	if err != nil {
+		panic(err)
+	}
 
 	server := syslog.NewServer()
 	server.SetFormat(syslog.Automatic)
 	server.SetHandler(handler)
-	err := server.ListenUDP("0.0.0.0:1514")
+	err = server.ListenUDP("0.0.0.0:1514")
 	if err != nil {
-		fmt.Println("Oups", err)
+		panic(err)
 	}
 	server.Boot()
 	server.Wait()
