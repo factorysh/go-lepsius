3ac922228b62c16ef2de69b53b8542b6e246f97b
Mathieu Lecarme <mlecarme@bearstech.com>
2017-10-12 00:17:05 +0200

    More apdex

--- reader/apdex.go
+++ reader/apdex.go
@@ -3,6 +3,7 @@ package reader
 // https://en.wikipedia.org/wiki/Apdex
 
 import (
+	"fmt"
 	_conf "github.com/bearstech/go-lepsius/conf"
 	"time"
 )
@@ -11,7 +12,15 @@ type Apdex struct {
 	t          int
 	status_key string
 	time_key   string
-	date_key   time.Time
+	date_key   string
+	events     chan *Event
+}
+
+type Event struct {
+	TimeStamp    *time.Time
+	Satisfied    uint32
+	Tolerating   uint32
+	NotSatisfied uint32
 }
 
 func (a *Apdex) Configure(conf map[string]interface{}) error {
@@ -28,10 +37,49 @@ func (a *Apdex) Configure(conf map[string]interface{}) error {
 	if err != nil {
 		return err
 	}
+	a.events = make(chan *Event)
+	a.date_key, _, err = _conf.ParseString(conf, "date_key", false)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (a *Apdex) Read(map[string]string) error {
-
+func (a *Apdex) Read(evt map[string]interface{}) error {
+	timestamp, _, err := _conf.ParseTime(evt, a.date_key, true)
+	if err != nil {
+		return err
+	}
+	status, _, err := _conf.ParseString(evt, a.status_key, true)
+	if len(status) != 3 {
+		return fmt.Errorf("Bad status lenght : %s", status)
+	}
+	event := &Event{
+		TimeStamp: timestamp,
+	}
+	switch s := status[0]; s {
+	case '1', '3', '4':
+		// this request is not interesting, nor its speed
+		return nil
+	case '5':
+		event.NotSatisfied += 1
+	case '2':
+		t, _, err := _conf.ParseInt(evt, a.time_key, true)
+		if err != nil {
+			return err
+		}
+		if t <= a.t {
+			event.Satisfied += 1
+		} else {
+			if t <= a.t*4 {
+				event.Tolerating += 1
+			} else {
+				event.NotSatisfied += 1
+			}
+		}
+	default:
+		return fmt.Errorf("Strange status : %s", status)
+	}
+	a.events <- event
 	return nil
 }
--- reader/apdex_test.go
+++ reader/apdex_test.go
@@ -1,1 +1,33 @@
 package reader
+
+import (
+	"testing"
+	"time"
+)
+
+func TestApdex(t *testing.T) {
+	a := &Apdex{}
+	err := a.Configure(map[string]interface{}{
+		"T":          500,
+		"status_key": "status",
+		"time_key":   "time",
+		"date_key":   "timestamp",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	go func() {
+		evt := <-a.events
+		t.Log("Event", evt)
+	}()
+	now := time.Now()
+	err = a.Read(map[string]interface{}{
+		"status":    "200",
+		"timestamp": &now,
+		"time":      342,
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	time.Sleep(1 * time.Second)
+}
