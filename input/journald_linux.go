7526e4f41124ece154b6be7d3650aa474599d3ea
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-12 11:39:17 +0100

    Read the journal

--- input/journald_linux.go
+++ input/journald_linux.go
@@ -1,27 +1,49 @@
 package input
 
 import (
+	"fmt"
 	"github.com/bearstech/go-lepsius/model"
 	"github.com/coreos/go-systemd/sdjournal"
 	"github.com/mitchellh/mapstructure"
+	"strings"
 	"time"
 )
 
 type Journald struct {
-	journald *sdjournal.Journal
+	journal *sdjournal.Journal
 }
 
-func (i *Journald) Lines() chan *model.Line {
+func (j *Journald) loop(lines chan *model.Line) {
+	for {
+		cur, err := j.journal.Next()
+		if err != nil {
+			panic(err)
+		}
+		if cur == 0 {
+			j.journal.Wait(1 * time.Second)
+		}
+		entry, err := j.journal.GetEntry()
+		if err != nil {
+			panic(err)
+		}
+		lines <- &model.Line{
+			Message: entry.Fields["MESSAGE"],
+		}
+	}
+}
+
+func (j *Journald) Lines() chan *model.Line {
 	lines := make(chan *model.Line)
+	go j.loop(lines)
 	return lines
 }
 
 type JournaldConf struct {
 	since   time.Duration
-	Matches []sdjournal.Match
+	Matches map[string]string
 }
 
-func (i *Journald) Configure(conf map[string]interface{}) error {
+func (j *Journald) Configure(conf map[string]interface{}) error {
 	var jconf JournaldConf
 	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
 		Result: &jconf,
@@ -30,6 +52,17 @@ func (i *Journald) Configure(conf map[string]interface{}) error {
 		return err
 	}
 	err = decoder.Decode(conf)
+	if err != nil {
+		return err
+	}
+	j.journal, err = sdjournal.NewJournal()
+	if err != nil {
+		return err
+	}
+	for key, value := range jconf.Matches {
+		j.journal.AddMatch(fmt.Sprintf("%v=%v", strings.ToUpper(key), value))
+	}
+	err = j.journal.SeekTail()
 	if err != nil {
 		return err
 	}
