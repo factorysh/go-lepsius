749816192d601048052ab5dc3d938cb0775c1131
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-13 22:20:23 +0100

    Yangtze POC

---
+++ filter/yantze.go
@@ -0,0 +1,34 @@
+package filter
+
+import (
+	"github.com/athoune/yangtze/index"
+	"github.com/mitchellh/mapstructure"
+)
+
+func init() {
+	register("yangtze", &Yangtze{})
+}
+
+type Yangtze struct {
+	config *YangtzeConf
+	index  *index.Index
+}
+
+type YangtzeConf struct {
+	Field   string
+	Pattern string
+}
+
+func (y *Yangtze) Configure(conf map[string]interface{}) error {
+	var cfg YangtzeConf
+	err := mapstructure.Decode(conf, &cfg)
+	if err != nil {
+		return err
+	}
+	y.config = &cfg
+	return nil
+}
+
+func (y *Yangtze) Filter(line map[string]interface{}) error {
+	return nil
+}
